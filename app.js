4fa2633a37f031ab5a179d99d41fbc6ca4e1058a
Nhi Danis <a.nhi1001@gmail.com>
2020-10-30 21:48:00 -0700

    final commit

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-$(document).ready(function() {
-    const currentDay = moment().format('dddd, MMMM Do YYYY');
-    $("#currentDay").text(currentDay);
-    
-    const currentTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-})
+$(document).ready(function () {
 
-currentTimes.forEach(time => {
-    const timeCheck = window.localStorage.getItem(time);
-    const currentHour = moment().hour()
-    const timeID = "#" + time
-
-    if(currentHour > time) {
-        $(timeID).addClass("past");
-        $(timeID).attr("disable", true);
-    } else if (currentHour === time) {
-        $(timeID).addClass("present");
-    }else {
-        $(timeID).addClass("future");
-    }
-
-    if (timeCheck === null)window.localStorage.setItem(time, "");
-    if (timeCheck.length > 0) $(timeID).attr("value", window.localStorage.getItem(time))
-})
-
-$("form").on("button", function(e) {
-    e.preventDefault();
-    const time = e.target.queryselector("input").getAttribute("id");
-    const text = e.target.queryselector("input"),value
-
-    window.localStorage.setItem(time, text);
-
-})
+   // Current day is displayed at the top of the calendar when the planner is open
+     const currentDay = moment().format('dddd, MMMM Do YYYY');
+     $("#currentDay").text(currentDay)
+     $("span").attr("style", "width: 75px")
+   
+   
+     const currentTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+   
+      currentTimes.forEach(time => {
+       const timeCheck = window.localStorage.getItem(time)
+       const currentHour = moment().hour()
+       const timeID = "#" + time
+       
+       // each time block is color-coded to indicate whether it is in the past, present, or future
+       if (currentHour > time) {
+         $(timeID).addClass("bg-secondary text-light")
+         $(timeID).attr("disabled", true)
+       } else if (currentHour === time) {
+         $(timeID).addClass("bg-danger text-light")
+       } else {
+         $(timeID).addClass("bg-success text-light")
+       }
+   
+       if (timeCheck === null) window.localStorage.setItem(time, "")
+       if (timeCheck.length > 0) $(timeID).attr("value", window.localStorage.getItem(time))
+     })
+   
+      // The text for that event is saved in local storage when the save button is click
+     $("form").on("button", function (e) {
+       e.preventDefault()
+   
+       const time = e.target.querySelector("input").getAttribute("id")
+       const text = e.target.querySelector("input").value
+   
+       window.localStorage.setItem(time, text)
+     })
+   })
+   
